2bad7135077b8ed7e22b77fcdc9494c5483544e9
Eduardo Malagutti <eduardosm2004@gmail.com>
2025-10-02 20:12:55 -0300

    feat: search

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
-import {Observable} from "rxjs";
-import {finalize} from "rxjs/operators";
+import {BehaviorSubject, combineLatest, Observable} from "rxjs";
+import {finalize, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +13,8 @@ import {finalize} from "rxjs/operators";
     </div>
     <div class="list">
       <label for="search">Search...</label>
-      <input id="search" type="text">
-      <app-progress-bar *ngIf="isLoading" ></app-progress-bar>
+      <input id="search" type="text" [ngModel]="search$ | async" (ngModelChange)="search$.next($event)">
+      <app-progress-bar *ngIf="isLoading"></app-progress-bar>
       <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
     </div>
   `,
@@ -22,12 +22,28 @@ import {finalize} from "rxjs/operators";
 })
 export class AppComponent {
 
+  readonly allTodos$: Observable<Todo[]>;
+
   readonly todos$: Observable<Todo[]>;
+
   isLoading = true;
 
+  search$ = new BehaviorSubject<string>('');
+
   constructor(todoService: TodoService) {
-    this.todos$ = todoService.getAll().pipe(
+    this.allTodos$ = todoService.getAll().pipe(
         finalize(() => this.isLoading = false)
     );
+
+      this.todos$ = combineLatest([
+          this.allTodos$,
+          this.search$
+      ]).pipe(
+          map(([todos, searchTerm]) =>
+              todos.filter(todo =>
+                  todo.task.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+          )
+      );
   }
 }
