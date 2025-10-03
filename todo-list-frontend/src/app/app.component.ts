560ec68905ec7c5c65bdf9bfd5ca7933e16fe539
Eduardo Malagutti <eduardosm2004@gmail.com>
2025-10-03 11:38:57 -0300

    refactor: rename todo attributes

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -15,35 +15,33 @@ import {finalize, map} from "rxjs/operators";
       <label for="search">Search...</label>
       <input id="search" type="text" [ngModel]="search$ | async" (ngModelChange)="search$.next($event)">
       <app-progress-bar *ngIf="isLoading"></app-progress-bar>
-      <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
+      <app-todo-item *ngFor="let todo of filteredTodos$ | async" [item]="todo"></app-todo-item>
     </div>
   `,
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
 
-  readonly allTodos$: Observable<Todo[]>;
-
-  readonly todos$: Observable<Todo[]>;
+  readonly filteredTodos$: Observable<Todo[]>;
 
   isLoading = true;
 
   search$ = new BehaviorSubject<string>('');
 
   constructor(todoService: TodoService) {
-    this.allTodos$ = todoService.getAll().pipe(
-        finalize(() => this.isLoading = false)
+    const allTodos$: Observable<Todo[]> = todoService.getAll().pipe(
+      finalize(() => this.isLoading = false)
     );
 
-      this.todos$ = combineLatest([
-          this.allTodos$,
-          this.search$
-      ]).pipe(
-          map(([todos, searchTerm]) =>
-              todos.filter(todo =>
-                  todo.task.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-          )
-      );
+    this.filteredTodos$ = combineLatest([
+      allTodos$,
+      this.search$
+    ]).pipe(
+      map(([todos, searchTerm]) =>
+        todos.filter(todo =>
+          todo.task.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      )
+    );
   }
 }
